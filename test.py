006b8345fe95d96e5d1dd536dbe2eb6832c124c8
Sabri <glad77fr@yahoo.fr>
2018-05-14 18:41:09 +0200

    f

--- test.py
+++ test.py
@@ -23,12 +23,27 @@ def bin_res(valeur):
     # Extraction des signes
     signe = []
     for i in valeur:
-        if i in ["*", "-"]:
+        if i in ["*", "-","~"]:
             signe.append(i)
     return signe
 
 
-def bin_count(val):
+def bin_count_or(val):
+
+    bin_list = list(itertools.product([0,1], repeat=val*2))
+    bin_list2=[]
+    res_list = []
+
+    for el in bin_list:
+        bin_list2.append(list(el))
+
+    for el in bin_list2:
+        if str(el).count("1") >= 1:
+            res_list.append(el)
+
+    return res_list
+
+def bin_count_xor(val):
 
     bin_list = list(itertools.product([0,1], repeat=val*2))
     bin_list2=[]
@@ -43,8 +58,9 @@ def bin_count(val):
 
     return res_list
 
-g=bin_count(4)
+g=bin_count_xor(1)
 
+print(g)
 res=[[1,0,1],[1,1,0]]
 
 def ins(list1,list2):
@@ -53,8 +69,10 @@ def ins(list1,list2):
         for i, el in enumerate(list1):
            for j, val in enumerate(list2):
                res.append(el+val)
+    return res
 
-ins(res,g)
+
+
 #g[1].insert(1,res[0])
 
 def convert_str(val):
@@ -70,16 +88,43 @@ def convert_str(val):
             if prev_val == "-":
                 resultat = resultat + "O" + str(cpt)
                 cpt = 0
+            if prev_val == "~":
+                resultat = resultat + "X" + str(cpt)
+                cpt=0
             resultat = resultat + '*'
             prev_val="*"
 
         if text[i] == "-":
+            if prev_val == "~":
+                resultat = resultat + "X" + str(cpt)
+                cpt = 0
             prev_val="-"
             cpt +=1
 
         if i == len(text)-1 and prev_val == "-":
             resultat = resultat + "O" + str(cpt)
 
-    print(resultat)
+        if text[i] =="~":
+            if prev_val =="-":
+                resultat = resultat + "O" + str(cpt)
+                cpt = 0
+            prev_val = "~"
+            cpt +=1
 
-convert_str("j*t-h-l-k-k**--")
+
+        if i == len(text)-1 and prev_val == "~":
+            resultat = resultat + "X" + str(cpt)
+
+
+
+
+convert_str("j*t-h-l-k~-k**---*~~--~~~~")
+
+j=[[0, 0, 0, 0, 0, 0, 0, 1],[0, 0, 0, 0, 0, 0, 0, 2]]
+l=[[1]]
+k=ins(l,j)
+f=ins([[0],[1]],k)
+
+
+print(k)
+print(f)
